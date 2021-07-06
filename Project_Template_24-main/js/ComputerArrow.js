19ab3993130a6e28bccb5fddec35077f964550e2
hardik962 <85226226+hardik962@users.noreply.github.com>
2021-07-06 22:05:55 +0530

    Add files via upload

---
+++ Project_Template_24-main/js/ComputerArrow.js
@@ -0,0 +1,26 @@
+class ComputerArrow {
+  constructor(x, y, width, height, archerAngle) {
+    var options = {
+       
+      isStatic: true
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image = loadImage("./assets/arrow.png");
+    World.add(world, this.body);
+    Matter.Body.setAngle(this.body, 6/ 2)
+  }
+
+ display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
